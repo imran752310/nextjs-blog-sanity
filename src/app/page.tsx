a1cd6f177a01db951b942235c0802a2fa44794c4
imran752310 <muhammadimran752310@gmail.com>
2025-05-20 15:40:25 +0500

    images gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import BlogCard from "@/components/BlogCard";
 import { client } from "../../sanity/lib/client";
 import { Post } from "../../typing";
 import Hero from "@/components/Hero";
+import Gallery from "@/components/Gallery";
 
 export const revalidate = 60; //seconds
 
@@ -12,26 +13,29 @@ export default async function Home() {
       "slug":slug.current
   }`;
 
-  const posts:Post[] = await client.fetch(query)
-  console.log(posts)
+  const posts: Post[] = await client.fetch(query);
+  console.log(posts);
 
   return (
     <>
-    <Hero />
-   
-    <main className="flex min-h-screen flex-col w-[90%] mx-auto ">
-      <h1 className="text-2xl font-bold uppercase my-20 text-center text-dark dark:text-light sm:text-3xl lg:text-5xl ">
-        Most Recent <span className="text-teal-600" >blogs</span>
-      </h1>
-      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
-        {
-          posts.map((post:Post)=>(
-            <BlogCard post={post} key={post.slug} />
-          ))
-        }
+      <Hero />
 
-      </section>
-    </main>
-     </>
+      <main className="flex min-h-screen flex-col w-[90%] mx-auto ">
+        <h1 className="text-2xl font-bold uppercase my-20 text-center text-dark dark:text-light sm:text-3xl lg:text-5xl ">
+          Most Recent <span className="text-teal-600">blogs</span>
+        </h1>
+        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+          {posts.map((post: Post) => (
+            <BlogCard post={post} key={post.slug} />
+          ))}
+        </section>
+
+        <div>
+          <div>
+            <Gallery />
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
---
+++ src/components/Gallery.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+const images = [
+  "/images/image1.jpg",
+  "/images/image2.jpg",
+  "/images/image3.jpg",
+  "/images/image4.jpg",
+  "/images/image5.jpg",
+  "/images/image6.jpg",
+  "/images/image7.jpg",
+  "/images/image8.jpg",
+];
+
+export default function Gallery() {
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl font-bold uppercase my-20 text-center text-dark dark:text-light sm:text-3xl lg:text-5xl ">
+          Our <span className="text-teal-600">Gallery</span>
+        </h1>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-4 p-4">
+        {images.map((src, index) => (
+          <div
+            key={index}
+            className="relative w-full h-64 rounded overflow-hidden shadow"
+          >
+            <Image
+              src={src}
+              alt={`Gallery image ${index + 1}`}
+              layout="fill"
+              objectFit="cover"
+              className="rounded"
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
