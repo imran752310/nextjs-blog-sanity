e70984ea6f94b97d31da5503a24f38d61dec5f03
imran752310 <muhammadimran752310@gmail.com>
2025-05-20 15:10:35 +0500

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,9 +19,9 @@ export default async function Home() {
     <>
     <Hero />
    
-    <main className="flex min-h-screen flex-col w-[100%] ">
-      <h1 className="text-2xl font-bold uppercase my-12 text-center text-dark dark:text-light sm:text-3xl lg:text-5xl ">
-        Most Recent blogs
+    <main className="flex min-h-screen flex-col w-[90%] mx-auto ">
+      <h1 className="text-2xl font-bold uppercase my-20 text-center text-dark dark:text-light sm:text-3xl lg:text-5xl ">
+        Most Recent <span className="text-teal-600" >blogs</span>
       </h1>
       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
         {
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,7 +2,7 @@
 
 export default function Container({children}:{children:React.ReactNode}) {
   return (
-    <div className="px-6 xs:px-8 sm:px-12  lg:px-10 xl:px-15 2xl:px-28">
+    <div className="">
       {children}
     </div>
   )
