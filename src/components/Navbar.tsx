0ade50318a7a1f2acc347aee147afdbc6b9f56c0
imran752310 <muhammadimran752310@gmail.com>
2025-05-20 15:59:59 +0500

    ok

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,19 +32,17 @@ export default function Navbar() {
               <Link href='/'
                 className="hover:text-teal-700 text-teal-700 block font-medium text-[15px]">Home</Link>
             </li>
-            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3"><a href='javascript:void(0)'
-              className="hover:text-teal-700 text-slate-900 block font-medium text-[15px]">Team</a>
-            </li>
-            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3"><a href='javascript:void(0)'
+
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3"><a href='/'
               className="hover:text-teal-700 text-slate-900 block font-medium text-[15px]">Feature</a>
             </li>
-            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3"><a href='javascript:void(0)'
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3"><a href='/'
               className="hover:text-teal-700 text-slate-900 block font-medium text-[15px]">Blog</a>
             </li>
-            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3"><a href='javascript:void(0)'
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3"><a href='/'
               className="hover:text-teal-700 text-slate-900 block font-medium text-[15px]">About</a>
             </li>
-            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3"><a href='javascript:void(0)'
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3"><a href='/'
               className="hover:text-teal-700 text-slate-900 block font-medium text-[15px]">Contact</a>
             </li>
           </ul>
